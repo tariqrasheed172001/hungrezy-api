596c3d5eb92e79fe9ebe23d1431e62f02fcb087c
tariqrasheed172001 <rasheedhadiq@gmail.com>
2023-07-26 18:16:43 +0530

    added Environment variables

--- connection.js
+++ connection.js
@@ -1,24 +1,15 @@
-// var mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//     host:"localhost",
-//     user:"root",
-//     password:"",
-//     database:"foodDelivery",
-// })
-
-// con.connect(function(error){
-//     if(error) throw error;
-
-//     console.log('connected');
-// });
-
-// module.exports = con;
 
 const {Sequelize} = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('foodDelivery','root','',{
-    host:'localhost',
+// const sequelize = new Sequelize('foodDelivery' ,'root','',{
+//     host:'localHost',
+//     dialect:'mysql',
+// })
+
+const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB ,process.env.MYSQL_ADDON_USER,process.env.MYSQL_ADDON_PASSWORD,{
+    host:process.env.MYSQL_ADDON_HOST,
     dialect:'mysql',
 })
 
