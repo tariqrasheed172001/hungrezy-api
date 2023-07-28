609ffb7744b520ced4aa8a31041097caa1788879
tariqrasheed172001 <rasheedhadiq@gmail.com>
2023-07-28 22:43:55 +0530

    ...

--- connection.js
+++ connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // const sequelize = new Sequelize('foodDelivery' ,'root','',{
 //     host:'localHost',
 //     dialect:'mysql',
-})
+// })
 
 const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB ,process.env.MYSQL_ADDON_USER,process.env.MYSQL_ADDON_PASSWORD,{
     host:process.env.MYSQL_ADDON_HOST,
